79a4a45d08a5a2897484b4a474ce5dde29b43d62
kongo2002 <kongo2002@googlemail.com>
2014-09-28 20:51:20 +0200

    move jira code into separate module

--- src/Gitlog.hs
+++ src/Gitlog.hs
@@ -3,15 +3,10 @@
 module Main where
 
 import           Control.Applicative
-import           Control.Exception  ( catch, SomeException(..) )
 
-import           Data.Aeson         ( decode )
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy as BL
-import           Data.List          ( intercalate )
 import           Data.Time.Clock    ( getCurrentTime )
-
-import           Network.HTTP.Conduit
 
 import           System.Console.GetOpt
 import           System.IO          ( hPutStrLn, stderr )
@@ -21,6 +16,7 @@ import           System.Exit        ( ExitCode(..), exitWith, exitSuccess )
 import           System.Environment ( getArgs )
 
 import           Gitlog.Encoder
+import           Gitlog.Jira
 import           Gitlog.Parser
 import           Gitlog.Types
 import           Gitlog.Utils
@@ -44,57 +40,8 @@ getGitOutput cfg args = do
   parse = return . filter noIntern . parseInput
   html x =
     if hasJira cfg
-      then toHtml cfg <$> populateTags cfg x
+      then toHtml cfg <$> getJiraInfo cfg x
       else return $ toHtml cfg x
-
-
-populateTags :: Config -> [GitEntry] -> IO [GitEntry]
-populateTags cfg = mapM go
- where
-  go entry =
-    case body of
-      [] -> return entry
-      ls -> do
-        body' <- mapM (safeFetch cfg) ls
-        return $ entry { gBody = body' }
-   where
-    body = gBody entry
-
-
-safeFetch :: Config -> GitBody -> IO GitBody
-safeFetch cfg tag =
-  fetch cfg tag `catch` ex
- where
-  ex (SomeException _) = return tag
-
-
-fetch :: Config -> GitBody -> IO GitBody
-fetch cfg (Tag ty no _) = do
-  mng <- newManager conduitManagerSettings
-  res <- decode <$> httpTimeout cfg mng url tout
-  return $ Tag ty no res
- where
-  tag  = BS.unpack ty ++ "-" ++ show no
-  base = cJira cfg
-  url  = base ++ "/rest/api/2/issue/" ++ tag ++ "?fields=" ++ fields
-  fields = intercalate ","
-    [ "summary"
-    , "customfield_10411"
-    ]
-
-  -- timeout in microseconds
-  tout = 1 * 1000 * 1000
-
-fetch _ x = return x
-
-
-httpTimeout :: Config -> Manager -> String -> Int -> IO BL.ByteString
-httpTimeout cfg manager url timeout = do
-  req <- applyBasicAuth user pw <$> parseUrl url
-  let req' = req {responseTimeout = Just timeout}
-  responseBody <$> httpLbs req' manager
- where
-  (Just (user, pw)) = cAuth cfg
 
 
 parseArgs :: [String] -> IO Config
---
+++ src/Gitlog/Jira.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Gitlog.Jira
+  ( getJiraInfo
+  ) where
+
+import           Control.Applicative
+import           Control.Exception ( catch, SomeException(..) )
+
+import           Data.Aeson        ( decode )
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Char8 as BS
+import           Data.List         ( intercalate )
+
+import           Network.HTTP.Conduit
+
+import           Gitlog.Types
+
+
+getJiraInfo :: Config -> [GitEntry] -> IO [GitEntry]
+getJiraInfo cfg = mapM go
+ where
+  go entry =
+    case body of
+      [] -> return entry
+      ls -> do
+        body' <- mapM (safeFetch cfg) ls
+        return $ entry { gBody = body' }
+   where
+    body = gBody entry
+
+
+safeFetch :: Config -> GitBody -> IO GitBody
+safeFetch cfg tag =
+  fetch cfg tag `catch` ex
+ where
+  ex (SomeException _) = return tag
+
+
+fetch :: Config -> GitBody -> IO GitBody
+fetch cfg (Tag ty no _) = do
+  mng <- newManager conduitManagerSettings
+  res <- decode <$> httpTimeout cfg mng url tout
+  return $ Tag ty no res
+ where
+  tag  = BS.unpack ty ++ "-" ++ show no
+  base = cJira cfg
+  url  = base ++ "/rest/api/2/issue/" ++ tag ++ "?fields=" ++ fields
+  fields = intercalate ","
+    [ "summary"
+    , "customfield_10411"
+    ]
+
+  -- timeout in microseconds
+  tout = 1 * 1000 * 1000
+
+fetch _ x = return x
+
+
+httpTimeout :: Config -> Manager -> String -> Int -> IO BL.ByteString
+httpTimeout cfg manager url timeout = do
+  req <- applyBasicAuth user pw <$> parseUrl url
+  let req' = req {responseTimeout = Just timeout}
+  responseBody <$> httpLbs req' manager
+ where
+  (Just (user, pw)) = cAuth cfg
+
+
+-- vim: set et sw=2 sts=2 tw=80:
